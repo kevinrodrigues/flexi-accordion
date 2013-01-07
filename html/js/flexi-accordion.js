6850d5b88ffce1a5bd18da72d036eacad68f593a
Kevin rodrigues <kevrodrigues116@gmail.com>
2013-01-07 17:00:14 +0000

    mod js files

--- html/js/flexi-accordion.js
+++ html/js/flexi-accordion.js
@@ -10,68 +10,86 @@ e: kevrodrigues116@gmail.com
             
             // overwrite defaults below in your .js file
             var defaults = {
-                speed:  400,                 // set your speed in m-seconds
-                isVertical: true      // change to vertical if required
+                speed:  400,          // set your speed in m-seconds
+                isVertical: true      // set to false if a horizontal accordion is required
             };
              
             o = $.extend(defaults, o);
             
             return this.each(function () {
 
+                // global variables
                 var $accordion = $(this).find('h3'),
                     $accordionOpen = $accordion.next().filter(':first'),
                     $accordionHeight = $accordionOpen.innerHeight(),
-                    $accordionWidth = $accordionOpen.innerWidth();
+                    $accordionWidth = $accordionOpen.innerWidth(),
+                    $zeroHeight = 0,
+                    $zeroWidth = 0;
 
+                // animates open/close function
                 function animateAccordion(el, animSize, isVertical) {
                     
                     var animStart,
                         animEnd;
+
                     if (isVertical) {
                         animStart = {width: 0};
                         animEnd = {width: animSize};
+
                     } else {
                         animStart = {height: 0};
                         animEnd = {height: animSize};
                     }
-
                     $accordionOpen.animate(animStart, {duration : o.speed});
                     $accordionOpen = $(el).next().css({'display' : 'block'}).animate(animEnd, { duration : o.speed});
-                    /*if (!isVertical) {
-                        $accordionOpen.animate({height : 0}, {duration : o.speed});
-                        $accordionOpen = $(el).next().css({'display' : 'block'}).animate({ height : animSize}, { duration : o.speed});
-
-                    } else {
-                        $accordionOpen.animate({width : 0}, {duration : o.speed});
-                        $accordionOpen = $(el).next().css({'display' : 'block'}).animate({ width : animSize}, { duration : o.speed});
-                    }*/
-
                 }
 
-                if  ($(this).length && o.orientation === false) {
-                    $accordion.next().filter(':not(:first)').css({'display' : 'none', height : 0});
+                // setting height/width to first element
+                function filterAccordion(dimention, el) {
 
-                    $accordion.click(function () {
-                        if ($accordionOpen.prev().get(0) == this) {
-                            return;
-                        }
-                        animateAccordion(this, $accordionHeight);
+                    if (o.isVertical) {
+                        dimention = {width: 0};
+                        $(el).parent().addClass('vertical-headers');
+
+                    } else {
+                        dimention = {height : 0};
+                    }
+
+                    $accordion.next().filter(':not(:first)').css(dimention, {'display' : 'none'});
+                }
+
+                // function accordionClicked(el, zero) {
+
+                //     if ($accordionOpen.prev().get(zero) == el) {
+                //         return;
+                //     }
+
+                // }
+
+                // start main plugin structure
+                if ($(this).length) {
                     
-                    });
+                    if (o.isVertical === false) {
+                        filterAccordion($zeroHeight);
 
-                } else if ($(this).length && o.isVertical === true) {
+                        $accordion.click(function () {
+                            // accordionClicked(this, 0);
+                            if ($accordionOpen.prev().get(0) == this) {
+                                return;
+                            }
+                            animateAccordion(this, $accordionHeight);
+                        });
+                    } else if (o.isVertical === true) {
+                        filterAccordion($zeroWidth, this);
 
-                    $accordion.next().filter(':not(:first)').css({'display' : 'none', width : 0});
-                    $(this).parent().addClass('vertical-headers');
-
-                    $accordion.click(function () {
-                        if ($accordionOpen.prev().get(0) == this) {
-                            return;
-                        }
-
-                        animateAccordion(this, $accordionWidth, o.isVertical);
-                        //$accordionOpen.animate({width : 0}, {duration : o.speed});
-                    });
+                        $accordion.click(function () {
+                            // accordionClicked(this, 0);
+                            if ($accordionOpen.prev().get(0) == this) {
+                                return;
+                            }
+                            animateAccordion(this, $accordionWidth, o.isVertical);
+                        });
+                    }
 
                 } else {
                     return;
